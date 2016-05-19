41883ddea5abfd3385e1e8c30b19982eccc0218f
Trezy <tre@trezy.com>
2016-05-18 19:51:28 -0500

    Cuz I wanna

--- js/collections/Base.js
+++ js/collections/Base.js
@@ -5,6 +5,11 @@ import Backbone from 'backbone'
 
 
 export default class Base extends Backbone.Collection {
+
+  /******************************************************************************\
+    Public Methods
+  \******************************************************************************/
+
   parse (response) {
     return response.data
   }
